d12266aa126872f0b36be5b5f0f00060066f5725
rkyxung <kimgy071920@gmail.com>
2025-06-02 05:50:53 +0900

    인터렉션 중

--- index.js
+++ index.js
@@ -1,10 +1,53 @@
-export function index () {
+export function index() {
   const navLogo = document.getElementById("navLogo");
+  const topBtn = document.getElementById("top");
+  const mainMockUp01 = document.getElementById("mainMockUp01");
+  const mainMockUp02 = document.getElementById("mainMockUp02");
+  const mainBadge01 = document.getElementById("Badge01");
+  const mainBadge02 = document.getElementById("Badge02");
+  const mainBadge03 = document.getElementById("Badge03");
+  const mainBadge04 = document.getElementById("Badge04");
+  const sw = document.querySelector('.scroll-wrapper');
+  console.log(sw.scrollHeight, sw.clientHeight);
 
   navLogo.addEventListener('mouseover', () => {
     navLogo.src = "img/navLogoHover.png";
-    navLogo.style.backgroundColor = "#111";
   });
+
+  navLogo.addEventListener('mouseleave', () => {
+    navLogo.src = "img/navLogo.png";
+  });
+
+  topBtn.addEventListener('mouseover', () => {
+    topBtn.src = "img/TopHover.png";
+  });
+
+  topBtn.addEventListener('mouseleave', () => {
+    topBtn.src = "img/Top.png";
+  });
+
+  topBtn.addEventListener("click", () => {
+    window.scrollTo({
+      top: 0, // 맨 위로
+      behavior: "smooth" // 스르륵 올라가게
+    });
+  });
+
+  mainMockUp01.addEventListener('animationend', () => {
+    mainMockUp01.style.opacity = "1";
+    mainMockUp02.style.opacity = "1";
+    mainBadge01.style.opacity = "1";
+    mainBadge02.style.opacity = "1";
+    mainBadge03.style.opacity = "1";
+    mainBadge04.style.opacity = "1";
+    mainBadge01.style.animation = "UpDown01 0.8s ease-in-out infinite alternate";
+    mainBadge02.style.animation = "UpDown02 0.8s ease-in-out infinite alternate";
+    mainMockUp01.style.animation = "MockUp01 0.8s ease-in-out infinite alternate";
+    mainMockUp02.style.animation = "MockUp02 0.8s ease-in-out infinite alternate";
+    mainBadge03.style.animation = "UpDown01 0.8s ease-in-out infinite alternate";
+    mainBadge04.style.animation = "UpDown02 0.8s ease-in-out infinite alternate";
+  });
+
 }
 
 index();
