6f351cbe082bee373e4e3ef091f1875f1c815af4
rkyxung <kimgy071920@gmail.com>
2025-06-04 00:38:47 +0900

    인터렉션

--- index.js
+++ index.js
@@ -41,10 +41,47 @@ export function index() {
   const onboarding = document.querySelector(".onboarding");
   const onboardingLine = document.getElementById("onboardingLine");
   const onboardingLineScreen = document.getElementById("onboardingLineScreen");
-
-
-
-
+  const onboardingTitle = document.querySelector("#onboardingTxt div");
+  const onboardingTxt = document.querySelector("#onboardingTxt p")
+  const onboardingMockup = document.getElementById("onboardingBg");
+  const Home = document.querySelector(".Home");
+  const HomeTitle = document.getElementById("HomeLabel");
+  const HomeTxt = document.getElementById("HomeTitle");
+  const HomeMockup = document.getElementById("HomeMockup");
+  const missionSectionTxt = document.getElementById("missionSectionTxt");
+  const missionLine = document.getElementById("missionLine");
+  const HomeLineGraph = document.getElementById("HomeLineGraph");
+  const HomeLineGraphTxt = document.getElementById("HomeLineGraphTxt");
+  const HomeGraphImg01 = document.getElementById("HomeGraph01");
+  const HomeGraphImg02 = document.getElementById("HomeGraph02");
+  const tipSectionTxt = document.getElementById("tipSectionTxt");
+  const tipLine = document.getElementById("tipLine");
+  const AIcamera = document.getElementById("cameraImg");
+  const AItitle = document.querySelector("#cameraTitle div");
+  const AItxt = document.querySelector("#cameraTitle p");
+  const MissionSectionTitle = document.getElementById("missionTitle");
+  const MissionSectionText = document.querySelector("#missionTitle p");
+  const missionImg03 = document.getElementById("mission03")
+  const missionDes03 = document.querySelector("#teamDes")
+  const missionImg02 = document.getElementById("mission02");
+  const missionDes02 = document.querySelector("#missionCenter .des");
+  const missionCenter = document.querySelector("#missionCenter");
+  const missionImg04 = document.getElementById("mission04");
+  const missionDes04 = document.getElementById("rankingDes");
+  const collectionMockup01 = document.getElementById("CollectionMockup01");
+  const collectionMockup02 = document.getElementById("CollectionMockup02");
+  const collectionTxt = document.getElementById("collectionDes");
+  const collectionImg01 = document.getElementById("collectionImg01");
+  const collectionImg02 = document.getElementById("collectionImg02");
+  const collectionImg03 = document.getElementById("collectionImg03");
+  const collectionImg04 = document.getElementById("collectionImg04");
+  const communityTitle = document.querySelector("#communityTitle");
+  const communityTxt = document.querySelector("#communityTitle p");
+  const communityImg = document.getElementById("communityImg");
+  const communityTxtImg = document.getElementById("communityTxt");
+  const myPageMockup = document.getElementById("myPageMockup");
+  const myPageTitle = document.querySelector("#myPageTitle div");
+  const myPageTxt = document.querySelector("#myPageTitle p");
 
   navLogo.addEventListener('mouseover', () => {
     navLogo.src = "img/navLogoHover.png";
@@ -230,18 +267,153 @@ export function index() {
         //온보딩
         if (entry.target.id === "onboardingLine") {
           onboardingLine.classList.remove('section');
-          // onboardingLineScreen.classList.add('onboardingLine');
+          onboardingLineScreen.classList.add('onboardingLine');
+          onboardingLineScreen.addEventListener('animationend', () => {
+            onboarding.classList.add('showDown');
+            onboardingTxt.style.opacity = '0';
+            onboardingMockup.style.opacity = '0';
+          })
+          onboarding.addEventListener('animationend', () => {
+            onboardingTxt.classList.add('symbolSloganShow');
+            onboardingTxt.style.animationDuration = '1s';
+          })
+          onboardingTxt.addEventListener('animationend', () => {
+            onboardingMockup.classList.add('onboardingMockupShow');
+          })
         }
 
-        // if (entry.target.classList.contains('onboarding')) {
-        //   onboarding.classList.add('show');
-        //   onboardingLine.style.opacity = '0';
-        //   onboarding.addEventListener('animationend', () => {
-        //     onboardingLine.classList.add('onboardingLine');
-        //   })
+        //홈섹션
+        if (entry.target.classList.contains('Home')) {
+          Home.classList.add('symbolSloganShow');
+          HomeTxt.style.opacity = '0';
+          missionSectionTxt.style.opacity = '0';
+          Home.addEventListener('animationend', () => {
+            HomeTxt.classList.add('symbolSloganShow');
+            HomeTxt.style.animationDuration = '1s';
+          })
+          HomeTxt.addEventListener('animationend', () => {
+            HomeMockup.classList.add('homeMockupShow');
+          })
+          HomeMockup.addEventListener('animationend', () => {
+            missionLine.classList.add('missionLineAni');
+            // setTimeout(() => {
+            //   missionSectionTxt.classList.add('symbolSloganShow');
+            // }, 400);
+          })
+          missionLine.addEventListener('animationend', () => {
+            missionSectionTxt.classList.add('symbolSloganShow');
+            missionSectionTxt.style.animationDuration = '0.5s';
+          });
+          missionSectionTxt.addEventListener('animationend', () => {
+            HomeLineGraph.classList.add('missionLineAni');
+          })
+          HomeLineGraph.addEventListener('animationend', () => {
+            HomeLineGraphTxt.classList.add('symbolSloganShow');
+            HomeLineGraphTxt.style.animationDuration = '0.5s'
+          })
+          HomeLineGraphTxt.addEventListener('animationend', () => {
+            HomeGraphImg01.classList.add('opacity');
+            HomeGraphImg01.style.animationDuration = '1s'
+            HomeGraphImg02.classList.add('opacity');
+            HomeGraphImg02.style.animationDuration = '1s'
+          })
+          HomeGraphImg02.addEventListener('animationend', () => {
+            tipLine.classList.add('missionLineAni');
+          })
+          tipLine.addEventListener('animationend', () => {
+            tipSectionTxt.classList.add('symbolSloganShow');
+            tipSectionTxt.style.animationDuration = '0.5s'
+          })
+        }
 
-        // }
+        //온보딩
+        if (entry.target.id === "cameraImg") {
+          AIcamera.classList.add('show');
+          AIcamera.addEventListener('animationend', () => {
+            AItitle.classList.add('missionLineAni');
+          })
+          AItitle.addEventListener('animationend', () => {
+            AItxt.classList.add('symbolSloganShow');
+          })
+        }
 
+        //미션섹션
+        if (entry.target.id === "missionTitle") {
+          MissionSectionTitle.classList.add('symbolSloganShow');
+          MissionSectionTitle.addEventListener('animationend', () => {
+            MissionSectionText.classList.add('symbolSloganShow');
+          })
+          MissionSectionText.addEventListener('animationend', () => {
+            missionImg03.classList.add('show');
+            missionImg03.addEventListener('animationend', () => {
+              missionDes03.classList.add('missionLineAni');
+              missionDes03.style.animationDuration = '0.7s';
+            })
+          })
+        }
+        if (entry.target.id === "missionCenter") {
+          missionCenter.classList.remove('section');
+          setTimeout(() => {
+            missionImg02.classList.add('show');
+          }, 20);
+          missionImg02.addEventListener('animationend', () => {
+            missionDes02.classList.add('missionLineAni');
+            missionDes02.style.animationDuration = '0.7s';
+          })
+        }
+        if (entry.target.id === "mission04") {
+          missionImg04.classList.add('show');
+          missionImg04.addEventListener('animationend', () => {
+            missionDes04.classList.add('missionLineAni');
+            missionDes04.style.animationDuration = '0.7s';
+          })
+        }
+        if (entry.target.id === "CollectionMockup01") {
+          collectionMockup01.classList.add('show');
+          collectionMockup01.addEventListener('animationend', () => {
+            collectionMockup02.classList.add('missionLineAni');
+            collectionMockup02.style.animationDuration = '0.9s';
+          })
+          collectionMockup02.addEventListener('animationend', () => {
+            collectionTxt.classList.add('symbolSloganShow');
+          })
+          collectionTxt.addEventListener('animationend', () => {
+            collectionImg01.classList.add('opacity');
+            collectionImg01.style.animationDuration = '0.6s';
+            setTimeout(() => {
+              collectionImg02.classList.add('opacity');
+              setTimeout(() => {
+                collectionImg03.classList.add('opacity');
+                setTimeout(() => {
+                  collectionImg04.classList.add('opacity');
+                }, 600)
+              }, 600)
+            }, 600)
+          })
+        }
+
+        //커뮤니티섹션
+        if (entry.target.id === "communityTitle") {
+          communityTitle.classList.add('symbolSloganShow');
+          communityTitle.addEventListener('animationend', () => {
+            communityTxt.classList.add('symbolSloganShow');
+          })
+          communityTxt.addEventListener('animationend', () => {
+            communityTxtImg.classList.add('show');
+            communityImg.classList.add('show');
+          })
+        }
+
+        //마이페이지섹션
+        if (entry.target.id === "myPageMockup") {
+          myPageTitle.classList.add('symbolSloganShow');
+          myPageTitle.addEventListener('animationend', () => {
+            myPageTxt.classList.add('symbolSloganShow');
+          })
+          myPageTxt.addEventListener('animationend', () => {
+            myPageMockup.classList.add('show');
+          })
+        }
       }
     });
   }, {
@@ -254,8 +426,8 @@ export function index() {
     observer.observe(el); // 해당 요소를 감시 대상으로 등록
   });
 
-  console.log(conceptTitle);
-  
+  console.log(MissionSection);
+
 
 }
 
